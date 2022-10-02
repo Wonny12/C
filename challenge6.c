af7504ae9be642e96f4d7557030ba49862a29df9
Wonny12 <wonny321@gmail.com>
2022-10-02 15:36:59 -0600

    2022-10-02

---
+++ challenge6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n,k;
+    int result=1;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for(k=1; result<=n; k++)
+    {
+        result *= 2;
+        printf("K:%d, result:%d, n:%d\n", k, result, n);
+    }
+    printf("K: %d\n", k-1);
+    return 0;
+}
