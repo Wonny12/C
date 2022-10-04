e146dcda7509a80bb39dfdb2df96e51b27e89df7
Wonny12 <wonny321@gmail.com>
2022-10-04 13:30:09 -0600

    Replacing files

--- comma_operator.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-    int num1 =1, num2 =3;
-    ++num1, num2++;
-    printf("Comma "), printf("Operator\n");
-    printf("num1: %d\n", num1), printf("num2: %d\n", num2);
-    return 0; 
-}
