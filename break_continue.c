fb3f73ec9b6dd6e9ce0e223f40b7f4e81265a5c0
Wonny12 <wonny321@gmail.com>
2022-10-04 14:22:40 -0600

    mult-table using break (print 2x1 & 2x2 only for table of 2)

---
+++ break_continue.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int i,j;
+    for (n=2; n<=9; n++)
+    {
+        for (i=1; i<=9; i++)
+        {
+            if (n==2)
+            {
+                for (j=1; j<=2; j++)
+                {
+                    printf("%d x %d = %d\n", n, j, n*j);
+                }
+                break;
+            }
+            else
+            {
+                printf("%d x %d = %d\n", n, i, n*i);
+            }
+        }
+    }
+    return 0;
+}
