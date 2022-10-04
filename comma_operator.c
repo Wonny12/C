c2fe912b9bc4703e6f8f7fb36f45afde2db4186a
Wonny12 <wonny321@gmail.com>
2022-10-04 15:10:06 -0600

    Comma Operator and ++,--operator

---
+++ comma_operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+    int num1 =1, num2 =3;
+    num1++,num2++;
+    printf("Comma "), printf("Operator\n");
+    printf("num1: %d\n", num1++), printf("num2: %d\n", num2);
+    printf("num1++: %d\n", num1);
+    return 0; 
+}
