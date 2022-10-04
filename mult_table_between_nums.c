e6fadab333fb225dac2af0828e6fcd727d4b5531
Wonny12 <wonny321@gmail.com>
2022-10-04 14:57:46 -0600

    mult table between two numbers

---
+++ mult_table_between_nums.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+void CalTimesTable(int n1, int n2)
+{
+    int i,j;
+    
+    if(n1<n2)
+    {
+        for(i=n1; i<=n2; i++)
+        {
+            for(j=1; j<=9; j++)
+            {
+                printf("%d x %d = %d\n", i,j,i*j);
+            }
+        }
+    }
+    else if (n1>n2)
+    {
+        for(i=n2; i<=n1; i++)
+        {
+            for(j=1; j<=9; j++)
+            {
+                printf("%d x %d = %d\n", i,j,i*j);            
+            }
+        }
+    }
+    
+}
+int main()
+{
+    int num,num2;
+    printf("Enter the two numbers: ");
+    scanf("%d %d", &num, &num2);
+    if(num==0 || num2==0)
+    {
+        printf("Re-enter the numbers: ");
+        scanf("%d %d", &num, &num2);
+    }
+    CalTimesTable(num, num2);
+    return 0;
+}
