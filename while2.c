dfc9bfcc7c5afd851d7baccdd72fb2f816c558b7
Wonny12 <wonny321@gmail.com>
2022-10-05 19:57:14 -0600

    Another while loop practice & calculating total the given 5 numbers

---
+++ while2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int n=0;
+    int i=0;
+    float total=0;
+    while(i<5)
+    {
+        while(n<=0)
+        {
+            printf("Enter the number: ");
+            scanf("%d", &n);
+            //printf("n: %d\n", n);
+        }
+        total+=n;
+        n=0;
+        i++;
+        
+    }
+    printf("Total: %0.2f\n", total);
+    return 0;
+}
